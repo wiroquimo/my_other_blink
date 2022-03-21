2b19e23d58d6a6789d4c4460a8847f561a0ece99
wiroquimo <rolandoquiroz@outlook.com>
2022-03-21 11:05:13 -0500

    change delay times to 1000

--- blink.ino
+++ blink.ino
@@ -25,7 +25,7 @@ void setup() {
  */
 void loop() {
     digitalWrite(led, HIGH);   /* turn the LED on (HIGH is the voltage level */
-    delay(3000);               /* wait for a second */
+    delay(1000);               /* wait for a second */
     digitalWrite(led, LOW);    /* turn the LED off by making the voltage LOW */
-    delay(3000);               /* wait for a second */
+    delay(1000);               /* wait for a second */
 }
